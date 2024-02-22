8e5932002b644a40b09f670a66fa733ff927ceae
Karl Jacques <karljacques95@gmail.com>
2024-02-22 17:09:07 +0000

    Add comment about CQRS

--- src/Ports/Web/IndexController.php
+++ src/Ports/Web/IndexController.php
@@ -15,6 +15,9 @@ final readonly class IndexController
     }
     public function __invoke(): string
     {
+        // I'd use a query here (CQRS query) rather than using repository directly.
+        // Potentially even split out a View object, rather than exposing Domain entities to the
+        // presentation layer.
         $listContactArr = $this->listContactRepository->findAll('created_at', 'DESC');
 
         return $this->renderer->render('list.php', [
